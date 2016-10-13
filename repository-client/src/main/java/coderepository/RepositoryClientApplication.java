0dcb05c18ba5109f82192127c58bdd994c81577e
= <david_sobreira_gouvea@epam.com>
2016-10-13 15:16:35 +0200

    Initial version

---
+++ repository-client/src/main/java/coderepository/RepositoryClientApplication.java
@@ -0,0 +1,12 @@
+package coderepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryClientApplication.class, args);
+	}
+}
